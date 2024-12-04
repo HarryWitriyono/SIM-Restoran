970f72d2cb9a98cc06964b0182cd025590184117
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2024-12-04 16:37:00 +0700

    Add files via upload

---
+++ IsiStruk.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: sql100.infinityfree.com
+-- Generation Time: Dec 04, 2024 at 04:35 AM
+-- Server version: 10.6.19-MariaDB
+-- PHP Version: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `if0_37747969_restoku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `IsiStruk`
+--
+
+CREATE TABLE `IsiStruk` (
+  `NoStruk` int(6) NOT NULL,
+  `IdPesanan` int(3) NOT NULL,
+  `JumlahPesanan` double(6,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `IsiStruk`
+--
+ALTER TABLE `IsiStruk`
+  ADD KEY `IndexNoStruk` (`NoStruk`),
+  ADD KEY `IndexIdPesanan` (`IdPesanan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
